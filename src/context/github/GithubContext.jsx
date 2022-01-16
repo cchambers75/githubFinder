94b61ed703afb21884f0023d005aeda6db407b64
Curtis Chambers <curtis.chambers@live.com>
2022-01-16 14:06:55 -0600

    added alert context. added single user page.

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -9,6 +9,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
+        user: {},
         loading: false
     }
 
@@ -35,6 +36,29 @@ export const GithubProvider = ({children}) => {
         })
     }
 
+    //get single user
+    const getUser = async (login) => {
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        if(response.status === 404){
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+    
+            dispatch({
+                type: 'GET_USER',
+                payload: data
+            })
+        }
+
+    }
+
     //clear users from state
     const clearUsers = () => {
         dispatch({type: 'CLEAR_USERS'})
@@ -42,7 +66,7 @@ export const GithubProvider = ({children}) => {
 
     const setLoading = () => dispatch({type: setLoading})
 
-    return <GithubContext.Provider value={{users: state.users, loading: state.loading, searchUsers, clearUsers}}>{children}</GithubContext.Provider>
+    return <GithubContext.Provider value={{users: state.users, user: state.user, loading: state.loading, searchUsers, clearUsers, getUser}}>{children}</GithubContext.Provider>
 }
 
 export default GithubContext
