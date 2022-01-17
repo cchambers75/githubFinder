188f5a825a832d8906a44585edc6a5cf97608802
Curtis Chambers <curtis.chambers@live.com>
2022-01-16 19:46:09 -0600

    progress save taking a break

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -4,9 +4,6 @@ import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
@@ -17,61 +14,7 @@ export const GithubProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    
-
-    //get single user
-    const getUser = async (login) => {
-        setLoading()
-
-        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
-
-        if(response.status === 404){
-            window.location = '/notfound'
-        } else {
-            const data = await response.json()
-    
-            dispatch({
-                type: 'GET_USER',
-                payload: data
-            })
-        }
-
-    }
-
-    //Get user repos
-    const getUserRepos = async (login) => {
-        setLoading()
-
-        const params = new URLSearchParams({
-            sort: 'created',
-            per_page: 10
-        })
-
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        })
-        const data = await response.json()
-
-        dispatch({
-            type: 'GET_REPOS',
-            payload: data
-        })
-    }
-
-    //clear users from state
-    const clearUsers = () => {
-        dispatch({type: 'CLEAR_USERS'})
-    }
-
-    const setLoading = () => dispatch({type: setLoading})
-
-    return <GithubContext.Provider value={{...state, dispatch, clearUsers, getUser, getUserRepos}}>{children}</GithubContext.Provider>
+    return <GithubContext.Provider value={{...state, dispatch}}>{children}</GithubContext.Provider>
 }
 
 export default GithubContext
